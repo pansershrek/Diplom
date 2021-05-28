1a16b06bc57000371f025d66ea0dd66c783f06a4
Skiba Gleb <g.skiba@corp.mail.ru>
2021-05-28 18:03:55 +0300

    ASD

--- main.py
+++ main.py
@@ -160,27 +160,27 @@ def main():
         [L_inf, "L_inf"],
     ]
     approximate_options71 = [
-        [approximate_options7_1, "WithoutNoise"],
+        #[approximate_options7_1, "WithoutNoise"],
         #[approximate_options7_1_white_noise, "WhiteNoise"],
         #[approximate_options7_1_gaussian_noise, "GaussianNoise"],
         #[approximate_options7_1_salt_and_papper_noise, "SaltAndPapperNoise"],
     ]
     approximate_options72 = [
-        #[approximate_options7_2, "WithoutNoise"],
+        [approximate_options7_2, "WithoutNoise"],
         #[approximate_options7_2_white_noise, "WhiteNoise"],
         #[approximate_options7_2_gaussian_noise, "GaussianNoise"],
         #[approximate_options7_2_salt_and_papper_noise, "SaltAndPapperNoise"],
     ]
     approximate_options73 = [
-        #[approximate_options7_3, "WithoutNoise"],
+        [approximate_options7_3, "WithoutNoise"],
         #[approximate_options7_3_white_noise, "WhiteNoise"],
         #[approximate_options7_3_gaussian_noise, "GaussianNoise"],
         #[approximate_options7_3_salt_and_papper_noise, "SaltAndPapperNoise"],
     ]
     all_approximate_options7 = [
-        #[approximate_options71, ApproximateFunction7_1, "Polinom"],
-        [approximate_options72, ApproximateFunction7_2, "Furie"],
-        [approximate_options73, ApproximateFunction7_3, "Exp"],
+        [approximate_options71, ApproximateFunction7_1, "Polinom"],
+        #[approximate_options72, ApproximateFunction7_2, "Furie"],
+        #[approximate_options73, ApproximateFunction7_3, "Exp"],
     ]
     for loss, loss_name in losses:
         for all_options, approximate_function, approximate_function_name in all_approximate_options7:
