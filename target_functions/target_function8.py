d942c504ca73b433fb8fcaa7bd842b914a242c36
Skiba Gleb <g.skiba@corp.mail.ru>
2021-06-07 20:19:50 +0300

    ASd

--- target_functions/target_function8.py
+++ target_functions/target_function8.py
@@ -13,3 +13,12 @@ class TargetFunction8(BaseTargetFunction):
         if self.x2y:
             return self.x2y[str(get_numpy_array(self.x))]
         return (self.x[0]**3 + tf.sin((self.x[0] + self.x[1]) * math.pi / 180) + math.e**(-self.x[2]) + 1 / (self.x[3]**2 + 4))
+
+
+class TargetFunction8_2(BaseTargetFunction):
+
+    def __call__(self):
+        """Сalculate function value"""
+        if self.x2y:
+            return self.x2y[str(get_numpy_array(self.x))]
+        return (math.e**(tf.sin(self.x[0] * math.pi / 180) + self.x[1]) + self.x[2]**4 - 16 * self.x[0] * self.x[1] * self.x[2])
