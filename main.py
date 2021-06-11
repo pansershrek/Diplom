0a3f3d190fba7fcb8098eb16798d4e7ee065bc29
Skiba Gleb <g.skiba@corp.mail.ru>
2021-06-11 03:30:48 +0300

    fix fork

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import logging
 import json
-import tensorflow as tf
 import math
 import os
 from utils.process import process_optimize, proccess_approximate
@@ -110,6 +109,7 @@ import numpy as np
 
 
 def get_metods(alphas):
+    import tensorflow as tf
     methods = []
     for alpha in alphas:
         methods.append(tf.keras.optimizers.SGD(learning_rate=alpha))
@@ -234,8 +234,6 @@ def approximate_example(args, f, target, opt, name=""):
 
 
 def main():
-    tf.debugging.set_log_device_placement(True)
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--ans', type=str, default="ans",
@@ -295,17 +293,25 @@ def main():
     print("axis_parallel_hyper_ellipsisoid_fnc")
     minimize_example(args, eval("axis_parallel_hyper_ellipsisoid_fnc"), option)
     """
-    alphas = [1000.0 / 10**x for x in range(8)]
-    methods = get_metods(alphas)
-
-    losses = [
-        [tf.keras.losses.MAE, "L_1"],
-        [rmse, "L_2"],
-        [L_inf, "L_inf"],
-    ]
 
     for loss, loss_name in losses:
         for p in [[4 + x, 2 + x, 2 + x] for x in range(0, 1)]:
+            import sys
+            print("BEGIN FORK", flush=True, file=sys.stderr)
+            pid = os.fork()
+            if pid != 0:
+                continue
+            import tensorflow as tf
+            tf.debugging.set_log_device_placement(True)
+            print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+            alphas = [1000.0 / 10**x for x in range(8)]
+            methods = get_metods(alphas)
+
+            losses = [
+                [tf.keras.losses.MAE, "L_1"],
+                [rmse, "L_2"],
+                [L_inf, "L_inf"],
+            ]
             ApproximateFunction7_1.P = p[0]
             ApproximateFunction7_2.P = p[1]
             ApproximateFunction7_3.P = p[2]
@@ -323,9 +329,6 @@ def main():
             ]
             for all_options, approximate_function, approximate_function_name in all_approximate_options7:
                 option, option_name = all_options
-                pid = os.fork()
-                if pid != 0:
-                    continue
                 res = approximate_example(
                     args, approximate_function, TargetFunction7,
                     option, f"Smoth_{loss_name}_{approximate_function_name}_{option_name}"
